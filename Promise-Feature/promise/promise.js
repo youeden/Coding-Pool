2c916b984c7881b6ff3f7f3ae278bfced0948bcf
youeden <vermogo@outlook.com>
2019-04-25 15:06:15 +0800

    use fetch get certain data

--- Promise-Feature/promise/promise.js
+++ Promise-Feature/promise/promise.js
@@ -1,30 +1,16 @@
-//1. 什么是Promise
-//2. 创建Promise的方式及区别
-//3. 
+let url = "http://www.wangshuwen.com/api/getRegion";
 
-let num = Math.floor(Math.random() * 10 ) + 1;
-// Promise is an [object]
-
-let count = (num) => { let abs = Math.abs(-121); num + abs};
-
- let promise = new Promise( (resolve, reject) => {
-    // do anything over here.
-    let abs = Math.abs(-1);
-    setTimeout( () => resolve(`${abs}`), 1000);//延时执行
- });
-
- promise.then( (result) => {
-     console.log(result);
- });
-
-
- let  promise2 = new Promise((resolve, reject) => {
-     resolve(count(num));//立即执行
- })
-
- promise2.then(result => {
-     console.log(result);
- })
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let str = JSON.stringify(data, null, "\t");
+        document.querySelector("#output pre").textContent = str;
+    })
+    .catch(error => {
+        let name = error.name;
+        let msg = error.msg;
+        console.log(`${name}, message is ${msg}`);
+    })
 
 
  
